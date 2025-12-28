1bed96cfa79e51043e316dc716331bf5d0a7fbe9
Thapakorn1 <116986095+Thapakorn1@users.noreply.github.com>
2025-12-28 17:58:04 +0700

    point

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 10000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ Connected to MongoDB!'))
+    .catch((err) => console.error('❌ MongoDB Connection Error:', err));
 
 app.get('/', (req, res) => res.send('Bot is online!'));
 app.listen(port, '0.0.0.0', () => {
